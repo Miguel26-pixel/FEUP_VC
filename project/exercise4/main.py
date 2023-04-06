6c420d858020564606b6599a635b99807886dea0
Miguel Amorim <migamorim2012@gmail.com>
2023-04-06 16:10:37 +0100

    Ex4 done need fix to work with any image shape

--- project/exercise4/main.py
+++ project/exercise4/main.py
@@ -1,50 +1,262 @@
+import matplotlib.pyplot as plt
 import cv2
+import os, glob
 import numpy as np
 
-img = cv2.imread('images/lane.png')
+def show_images(images, cmap=None):
+    cols = 2
+    rows = (len(images)+1)//cols
+    
+    plt.figure(figsize=(10, 11))
+    for i, image in enumerate(images):
+        plt.subplot(rows, cols, i+1)
+        # use gray scale color map if there is only one channel
+        cmap = 'gray' if len(image.shape)==2 else cmap
+        plt.imshow(image, cmap=cmap)
+        plt.xticks([])
+        plt.yticks([])
+    plt.tight_layout(pad=0, h_pad=0, w_pad=0)
+    plt.show()
 
-#manual segmentation
-roi_vertices = [(0, img.shape[0]), (img.shape[1] / 2, img.shape[0] / 2), (img.shape[1], img.shape[0])]
-mask = np.zeros_like(img)
-cv2.fillPoly(mask, np.int32([roi_vertices]), (255, 255, 255))
-masked_img = cv2.bitwise_and(img, mask)
+test_images = [plt.imread(path) for path in glob.glob('images/test_images/*.jpg')]
 
-# Convert the image to grayscale
-gray_img = cv2.cvtColor(masked_img, cv2.COLOR_BGR2GRAY)
 
-# Apply Gaussian blur to the image to remove noise
-blur_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
+# selecting only yellow and white colors in the images using the RGB channels 
 
-# Apply Canny edge detection
-canny_img = cv2.Canny(blur_img, 100, 200)
+def select_rgb_white_yellow(image): 
+    # white color mask
+    lower = np.uint8([200, 200, 200])
+    upper = np.uint8([255, 255, 255])
+    white_mask = cv2.inRange(image, lower, upper)
+    # yellow color mask
+    lower = np.uint8([190, 190,   0])
+    upper = np.uint8([255, 255, 255])
+    yellow_mask = cv2.inRange(image, lower, upper)
+    # combine the mask
+    mask = cv2.bitwise_or(white_mask, yellow_mask)
+    masked = cv2.bitwise_and(image, image, mask = mask)
+    return masked
 
-# Apply color thresholding to segment the lane lines
-lower_white = np.array([200, 200, 200])
-upper_white = np.array([255, 255, 255])
-white_mask = cv2.inRange(masked_img, lower_white, upper_white)
 
-combined_mask = cv2.bitwise_or(canny_img, white_mask)
+# RGB images are converted into HSV color space
 
-# Define the Hough transform parameters
-rho = 2
-theta = np.pi / 180
-threshold = 100
-min_line_len = 100
-max_line_gap = 75
+def convert_hsv(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 
-# Run Hough on combined mask
-lines = cv2.HoughLinesP(white_mask, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
 
-for x in range(0, len(lines)):
-    for x1,y1,x2,y2 in lines[x]:
-        cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+# RGB images are converted into HSL color space
 
-# Display the output
-cv2.imshow('Original Image', img)
-# cv2.imshow('Masked Image', masked_img)
-# cv2.imshow('Canny Image', canny_img)
-cv2.imshow('White Mask', white_mask) # best results
-# cv2.imshow('Combined Mask', combined_mask)
-#cv2.imshow('lines', lines)
+def convert_hsl(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+
+
+def select_white_yellow(image):
+    converted = convert_hsl(image)
+    # white color mask
+    lower = np.uint8([  0, 200,   0])
+    upper = np.uint8([255, 255, 255])
+    white_mask = cv2.inRange(converted, lower, upper)
+    # yellow color mask
+    lower = np.uint8([ 10,   0, 100])
+    upper = np.uint8([ 40, 255, 255])
+    yellow_mask = cv2.inRange(converted, lower, upper)
+    # combine the mask
+    mask = cv2.bitwise_or(white_mask, yellow_mask)
+    return cv2.bitwise_and(image, image, mask = mask)
+
+white_yellow_images = list(map(select_white_yellow, test_images))
+
+
+def convert_gray_scale(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+gray_images = list(map(convert_gray_scale, white_yellow_images))
+
+
+def apply_smoothing(image, kernel_size=15):
+    
+    return cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
+
+blurred_images = list(map(lambda image: apply_smoothing(image), gray_images))
+
+
+def detect_edges(image, low_threshold=50, high_threshold=150):
+    return cv2.Canny(image, low_threshold, high_threshold)
+
+edge_images = list(map(lambda image: detect_edges(image), blurred_images))
+
+
+def filter_region(image, vertices):
+    mask = np.zeros_like(image)
+    if len(mask.shape)==2:
+        cv2.fillPoly(mask, vertices, 255)
+    else:
+        cv2.fillPoly(mask, vertices, (255,)*mask.shape[2]) # in case, the input image has a channel dimension        
+    return cv2.bitwise_and(image, mask)
+
+    
+def select_region(image):
+    # first, define the polygon by vertices
+    rows, cols = image.shape[:2]
+    bottom_left  = [cols*0.1, rows*0.95]
+    top_left     = [cols*0.4, rows*0.6]
+    bottom_right = [cols*0.9, rows*0.95]
+    top_right    = [cols*0.6, rows*0.6] 
+    # the vertices are an array of polygons (i.e array of arrays) and the data type must be integer
+    vertices = np.array([[bottom_left, top_left, top_right, bottom_right]], dtype=np.int32)
+    return filter_region(image, vertices)
+
+
+# images showing the region of interest only
+roi_images = list(map(select_region, edge_images))
+
+
+def hough_lines(image):
+
+    return cv2.HoughLinesP(image, rho=1, theta=np.pi/180, threshold=20, minLineLength=20, maxLineGap=300)
+
+
+list_of_lines = list(map(hough_lines, roi_images))
+# draw lines on actutal image
+
+def draw_lines(image, lines, color=[255, 0, 0], thickness=2, make_copy=True):
+    # the lines returned by cv2.HoughLinesP has the shape (-1, 1, 4)
+    if make_copy:
+        image = np.copy(image) # don't want to modify the original
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(image, (x1, y1), (x2, y2), color, thickness)
+    return image
+
+
+line_images = []
+for image, lines in zip(test_images, list_of_lines):
+    line_images.append(draw_lines(image, lines))
+
+
+### Averaging and Extrapolating Lines : averaged multiple lines detected for a lane line
+
+def average_slope_intercept(lines):
+    left_lines    = [] # (slope, intercept)
+    left_weights  = [] # (length,)
+    right_lines   = [] # (slope, intercept)
+    right_weights = [] # (length,)
+    
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            if x2==x1:
+                continue # ignore a vertical line
+            slope = (y2-y1)/(x2-x1)
+            intercept = y1 - slope*x1
+            length = np.sqrt((y2-y1)**2+(x2-x1)**2)
+            if slope < 0: # y is reversed in image
+                left_lines.append((slope, intercept))
+                left_weights.append((length))
+            else:
+                right_lines.append((slope, intercept))
+                right_weights.append((length))
+    
+    # add more weight to longer lines    
+    left_lane  = np.dot(left_weights,  left_lines) /np.sum(left_weights)  if len(left_weights) >0 else None
+    right_lane = np.dot(right_weights, right_lines)/np.sum(right_weights) if len(right_weights)>0 else None
+    
+    return left_lane, right_lane # (slope, intercept), (slope, intercept)
+
+
+#   Convert a line represented in slope and intercept into pixel points
+
+def make_line_points(y1, y2, line):
+
+    if line is None:
+        return None
+
+    slope, intercept = line
+    
+    # make sure everything is integer as cv2.line requires it
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    y1 = int(y1)
+    y2 = int(y2)
+    
+    return ((x1, y1), (x2, y2))
+
+
+def lane_lines(image, lines):
+    left_lane, right_lane = average_slope_intercept(lines)
+    
+    y1 = image.shape[0] # bottom of the image
+    y2 = y1*0.6         # slightly lower than the middle
+
+    left_line  = make_line_points(y1, y2, left_lane)
+    right_line = make_line_points(y1, y2, right_lane)
+    
+    return left_line, right_line
+
+    
+def draw_lane_lines(image, lines, color=[255, 0, 0], thickness=10):
+    # make a separate image to draw lines and combine with the orignal later
+    line_image = np.zeros_like(image)
+    for line in lines:
+        if line is not None:
+            cv2.line(line_image, *line,  color, thickness)
+    # image1 * α + image2 * β + λ
+    # image1 and image2 must be the same shape.
+    return cv2.addWeighted(image, 1.0, line_image, 0.95, 0.0)
+
+def bresenham_line(x0, y0, x1, y1):
+    dx, dy = abs(x1 - x0), abs(y1 - y0)
+    sx, sy = 1 if x0 < x1 else -1, 1 if y0 < y1 else -1
+    err = dx - dy
+    points = []
+    while True:
+        points.append((x0, y0))
+        if x0 == x1 and y0 == y1:
+            break
+        e2 = 2 * err
+        if e2 > -dy:
+            err -= dy
+            x0 += sx
+        if e2 < dx:
+            err += dx
+            y0 += sy
+    return points
+
+def draw_dotted_line(img, lines, color=(0, 255, 255), thickness=4, gap=10):
+    line_image = np.zeros_like(image)
+    for line in lines:
+        print(line)
+        x,y = line
+        print(x,y)
+        points = bresenham_line(x[0], x[1], y[0], y[1])
+        n_points = len(points)
+        draw = True
+        for i in range(n_points):
+            if draw:
+                cv2.circle(img, points[i], thickness //
+                        2, color=color, thickness=-1)
+            if i % gap == 0:
+                draw = not draw
+    return cv2.addWeighted(image, 1.0, line_image, 0.95, 0.0)
+                
+        
+
+
+lane_images = []
+for image, lines in zip(test_images, list_of_lines):
+
+    h,w,r = image.shape
+    lane_width_px = w*2/3
+    src = np.array([[200, 720], [1100, 720], [750, 470], [550, 470]], np.float32)
+    dst = np.array([[350, 720], [950, 720], [950, 0], [350, 0]], np.float32)
+
+
+    M = cv2.getPerspectiveTransform(src, dst)
+
+    output = cv2.warpPerspective(draw_dotted_line(image, lane_lines(image, lines)), M, (w, h), flags=cv2.INTER_LINEAR)
+
+    lane_images.append(output)
+
+    
+show_images(lane_images)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
